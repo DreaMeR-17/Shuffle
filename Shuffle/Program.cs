bcf29a4352c79a952094c82278e84571ade2bb78
DreaMeR-17 <pronka_1993@mail.ru>
2024-08-05 11:54:38 +0600

    Добавьте файлы проекта.

---
+++ Shuffle/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Linq;
+
+namespace Shuffle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] values = new int[10];
+
+            values = GetValueForArrayElement(values);
+            ShowArrayElements(values);
+
+            Console.WriteLine();
+
+            values = Shuffle(values);
+            ShowArrayElements(values);
+        }
+
+        static int[] GetValueForArrayElement(int[] values)
+        {
+            int startValue = 0;
+
+            for (int i = 0; i < values.Length; i++)
+            {
+                values[i] = startValue++;
+            }
+
+            return values;
+        }
+
+        static void ShowArrayElements(int[] values)
+        {
+            for (int i = 0; i < values.Length; i++)
+            {
+                Console.Write(values[i] + " ");
+            }
+        }
+
+        static int[] Shuffle(int[] values)
+        {
+            Random random = new Random();
+
+            int arrayLenght = values.Count();
+
+            while (arrayLenght > 1)
+            {
+                arrayLenght--;
+
+                int randomIndex = random.Next(arrayLenght);
+                int temp = values[randomIndex];
+                values[randomIndex] = values[arrayLenght];
+                values[arrayLenght] = temp;
+            }
+
+            return values;
+        }
+    }
+}
